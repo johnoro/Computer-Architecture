b35ba4462db79607a0298dd96ccbad0b5317b48f
John O'Rourke <officialjohnorourke@gmail.com>
2019-06-14 12:12:29 -0400

    Fixes rest of merge mishaps

--- asm/curve.asm
+++ asm/curve.asm
@@ -5,16 +5,18 @@
     LDI R3, PrintAsterisk
     LDI R4, Loop
 
-    CALL R4
+    LDI R0, FirstLoop
+    CALL R0
 
     HLT
 
 Loop:
+    LDI R0, 10 ; newline's ASCII value
+    PRA R0
+FirstLoop:
     LDI R0, 64 ; end of loop
     CMP R1, R0
     ADD R1, R1
-    LDI R0, 10 ; newline's ASCII value
-    PRA R0
     LDI R0, 42 ; * in ASCII
     JLE R3
     RET
